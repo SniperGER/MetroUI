463462d2152cf46ec464b9d8a9f019d475ff0799
SniperGER <janikschmidt@Janiks-Mac-mini.local>
2014-05-02 18:33:15 +0200

    Added MetroUI JS module, better examples, various improvements

--- javascript/MetroUI.js
+++ javascript/MetroUI.js
@@ -1,15 +1,30 @@
 var touchEventStart = (('ontouchend' in window)) ? 'touchstart' : 'mousedown';
 var touchEventEnd = (('ontouchend' in window)) ? 'touchend' : 'click';
 
-$('div[data-id=switch1]').bind(touchEventEnd, function() {
-	var $checkboxElement = $('input[type=checkbox][data-id=' + $(this).attr("data-id") + ']');
+var MetroUI = {
+	init: function(options) {
 
-	$(this).toggleClass("checked");
-	$checkboxElement.attr("checked", !$checkboxElement.attr("checked"));
-	
-	if ($checkboxElement.attr("checked") == "checked") {
-		$('p#checkbox1checked').html("Checkbox 1 is: " + $checkboxElement.attr("checked"));
-	} else {
-		$('p#checkbox1checked').html("Checkbox 1 is: unchecked");
+		var options = options || {},
+			objID = options.objID || undefined,
+			id = options.id || undefined,
+			startEnabled = options.startEnabled || false;
+		
+		if (!$(objID).hasClass("disabled")) {
+			$(objID).attr("data-id",id);
+			$('body').append("<input class=\"metro\" type=\"checkbox\" data-id=\"" + id + "\">");
+			$(objID).bind(touchEventEnd, function() {
+				var checkboxElement = $('input[type=checkbox][data-id=' + $(this).attr("data-id") + ']');
+		
+				$(this).toggleClass("checked");
+				checkboxElement.attr("checked", !checkboxElement.attr("checked"));
+			});
+			
+			if(startEnabled) {
+				$(objID).addClass("checked");
+				$('input[type=checkbox][data-id=' + id + ']').attr("checked", "checked");
+			} else {
+				$(objID).removeClass("checked");
+			}
+		}
 	}
-});
+}
