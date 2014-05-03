3bad9c7c371f2554a25904463f7f89c5ee2bac10
SniperGER <janikschmidt@icloud.com>
2014-05-03 12:07:58 +0200

    new features, added playground

--- javascript/MetroUI.js
+++ javascript/MetroUI.js
@@ -2,28 +2,43 @@ var touchEventStart = (('ontouchend' in window)) ? 'touchstart' : 'mousedown';
 var touchEventEnd = (('ontouchend' in window)) ? 'touchend' : 'click';
 
 var MetroUI = {
-	init: function(options) {
+	create: function(options) {
 
 		var options = options || {},
+			type = options.type || undefined,
 			objID = options.objID || undefined,
 			id = options.id || undefined,
-			startEnabled = options.startEnabled || false;
+			active = options.active || false,
+			startSwitched = options.startSwitched || false,
+			callback = options.callback || undefined;
 		
-		if (!$(objID).hasClass("disabled")) {
-			$(objID).attr("data-id",id);
-			$('body').append("<input class=\"metro\" type=\"checkbox\" data-id=\"" + id + "\">");
-			$(objID).bind(touchEventEnd, function() {
-				var checkboxElement = $('input[type=checkbox][data-id=' + $(this).attr("data-id") + ']');
+		if (type == "button") {
+			$(objID).addClass("metro");
+			if (active == true && callback != undefined) {
+				$(objID).bind(touchEventEnd, callback);
+			}
+			if (active == false) {
+				$(objID).attr("disabled","disabled");
+			}
+		}
 		
-				$(this).toggleClass("checked");
-				checkboxElement.attr("checked", !checkboxElement.attr("checked"));
-			});
+		if (type == "switch") {
+			if (!$(objID).hasClass("disabled")) {
+				$(objID).attr("data-id",id);
+				$('body').append("<input class=\"metro\" type=\"checkbox\" data-id=\"" + id + "\">");
+				$(objID).bind(touchEventEnd, function() {
+					var checkboxElement = $('input[type=checkbox][data-id=' + $(this).attr("data-id") + ']');
 			
-			if(startEnabled) {
-				$(objID).addClass("checked");
-				$('input[type=checkbox][data-id=' + id + ']').attr("checked", "checked");
-			} else {
-				$(objID).removeClass("checked");
+					$(this).toggleClass("checked");
+					checkboxElement.attr("checked", !checkboxElement.attr("checked"));
+				});
+				
+				if(startSwitched) {
+					$(objID).addClass("checked");
+					$('input[type=checkbox][data-id=' + id + ']').attr("checked", "checked");
+				} else {
+					$(objID).removeClass("checked");
+				}
 			}
 		}
 	}
